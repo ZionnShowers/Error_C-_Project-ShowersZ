67f8fa9bdf76e021ad6bd12f54d83e5448b91eec
ZionnShowers <zionn.d.showers@gmail.com>
2026-09-14 14:17:59 -0700

    Added my name

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+//Zionn Showers
+//9-14-2026
+//Check reflection and bug fixes
 //Peer Review: Valery Lot
 //Review: Code worked up to Section 10. Bug log looks clean and organized. Didn't seem to get a chance to finish the reflection part yet.
 // ============================================================
